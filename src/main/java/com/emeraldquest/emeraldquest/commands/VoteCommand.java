a942fef9e2ecfa71471d8b4401debb65d70faf34
BitcoinJake09 <54378383+BitcoinJake09@users.noreply.github.com>
2020-04-22 19:20:36 -0400

    vote and rewards

---
+++ src/main/java/com/emeraldquest/emeraldquest/commands/VoteCommand.java
@@ -0,0 +1,34 @@
+package com.emeraldquest.emeraldquest.commands;
+
+import com.emeraldquest.emeraldquest.EmeraldQuest;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Set;
+
+
+public class VoteCommand extends CommandAction {
+  private EmeraldQuest emeraldQuest;
+
+  public VoteCommand(EmeraldQuest plugin) {
+    emeraldQuest = plugin;
+  }
+    public boolean run(CommandSender sender, Command cmd, String label, String[] args, Player player) {
+	String playerName = player.getName(); //(player.getName()
+	if (emeraldQuest.didVote(playerName) == 0) {
+		player.sendMessage(ChatColor.GREEN + "Please Vote here for Rewards! https://minecraft-mp.com/server/189942/vote/");
+		player.sendMessage(ChatColor.AQUA + "Run command again after you vote for reward!");
+	} else if (emeraldQuest.didVote(playerName) == 1) {
+		if (emeraldQuest.giveVoteReward(player) == true){
+		//player.sendMessage(ChatColor.GREEN + "You just got a reward for voting!");
+		int claimed = emeraldQuest.claimVote(playerName);
+		}
+
+	} else if (emeraldQuest.didVote(playerName) == 2) {
+		player.sendMessage(ChatColor.GREEN + "You've already voted, try again later please!");
+	} 
+        return true;
+    }
+}
